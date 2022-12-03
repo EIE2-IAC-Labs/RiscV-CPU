eb5a610cc92d8b3049a9952a2d9bbaa4abe8ed25
Ana Dimoska <ad2121@ic.ac.uk>
2022-12-03 14:18:56 +0000

    ram/obj_dir is deleted

--- ram/obj_dir/testbench_ram.d
+++
@@ -1,9 +0,0 @@
-testbench_ram.o: ../testbench_ram.cpp Vram.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_funcs.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_vcd_c.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_trace.h \
-  /usr/local/Cellar/verilator/4.228/share/verilator/include/verilated_trace_defs.h
