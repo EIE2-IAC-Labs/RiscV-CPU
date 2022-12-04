a68fd23307b7b2a51908332a41e2fde127e7b7b4
Nik <nl621@ic.ac.uk>
2022-12-04 12:05:46 +0000

    Minor syntax fixes.
    Co-authored by: elkouny <108074024+elkouny@users.noreply.github.com>

--- branchcontrol.sv
+++ branchcontrol.sv
@@ -1,30 +1,29 @@
 module branchcontrol #(
     parameter DW=32
 ) (
-    input logic [DW-1:0] rd1,
-    input logic [DW-1:0] rd2,
-    input logic func3,
-    output logic branch //1 or 0 deciding if we branch or not
+    input logic [DW-1:0]    rd1_i,
+    input logic [DW-1:0]    rd2_i,
+    input logic [2:0]       funct3_i,
+    output logic            branch_o //1 or 0 deciding if we branch_o or not
 );
 
 always_comb begin
-    case (func3)
-    3'b000:branch = rd1==rd2 ? 1'b1:1'b0; //beq if rd1 equal rd2
-    3'b001:branch = rd1!=rd2 ? 1'b1:1'b0; //bne if rd1 not equal rd2
-    3'b100:branch = rd1<rd2 ? 1'b1:1'b0; //blt branch if rd1 less than rd2
-    3'b101:branch = rd1>=rd2 ? 1'b1:1'b0; //bge branch if rd1 greater than or equal than rd2
-    3'b110:begin
-        rd1={{rd1[31]},{rd1}}; //sign extend rd1 by 1 bit
-        rd2={{1'b0},{rd2}}; //make rd2 unsigned
-        branch = rd1<rd2 ? 1'b1:1'b0; //bltu branch if rd2 unisigned less than rd1
-    end
-    3'b111:begin
-        rd1={{rd1[31]},{rd1}}; //sign extend rd1 by 1 bit
-        rd2={{1'b0},{rd2}}; //make rd2 unsigned
-        branch = rd1>=rd2 ? 1'b1:1'b0; //bgeu branch if rd2 unisigned greater than rd1
-    end
-    default:branch=1'b0;
-        
+    case (funct3_i)
+        3'b000: branch_o = rd1_i == rd2_i ? 1'b1 : 1'b0; //beq if rd1_i equal rd2_i
+        3'b001: branch_o = rd1_i != rd2_i ? 1'b1 : 1'b0; //bne if rd1_i not equal rd2_i
+        3'b100: branch_o = rd1_i < rd2_i ? 1'b1 : 1'b0; //blt branch_o if rd1_i less than rd2_i
+        3'b101: branch_o = rd1_i >= rd2_i ? 1'b1 : 1'b0; //bge branch_o if rd1_i greater than or equal than rd2_i
+        3'b110: begin
+            rd1_i = {{rd1_i[31]},{rd1_i}}; //sign extend rd1_i by 1 bit
+            rd2_i = {{1'b0},{rd2_i}}; //make rd2_i unsigned
+            branch_o = rd1_i < rd2_i ? 1'b1 : 1'b0; //bltu branch_o if rd2_i unisigned less than rd1_i
+        end
+        3'b111: begin
+            rd1_i = {{rd1_i[31]},{rd1_i}}; //sign extend rd1_i by 1 bit
+            rd2_i = {{1'b0},{rd2_i}}; //make rd2_i unsigned
+            branch_o = rd1_i >= rd2_i ? 1'b1 : 1'b0; //bgeu branch_o if rd2_i unisigned greater than rd1_i
+        end
+        default: branch_o=1'b0;    
     endcase
 end
     
