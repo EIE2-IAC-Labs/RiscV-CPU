233e3937c19c1a7c118f97bd39a1b534c2bed252
elkouny <108074024+elkouny@users.noreply.github.com>
2022-12-04 13:01:08 +0000

    fixed signed branch with alu code
    Co-authored-by: BenShort <benedictnjshort@gmail.com>

--- branchcontrol.sv
+++ branchcontrol.sv
@@ -11,18 +11,22 @@ always_comb begin
     case (funct3_i)
         3'b000: branch_o = rd1_i == rd2_i ? 1'b1 : 1'b0; //beq if rd1_i equal rd2_i
         3'b001: branch_o = rd1_i != rd2_i ? 1'b1 : 1'b0; //bne if rd1_i not equal rd2_i
-        3'b100: branch_o = rd1_i < rd2_i ? 1'b1 : 1'b0; //blt branch_o if rd1_i less than rd2_i
+        3'b100: begin
+                    if( rd1_i == rd2_i) branch_o = 0;
+                    else if ((rd1_i [DW - 1] == 0'b0) && (rd2_i [DW - 1] == 0'b1)) branch_o = 0;
+                    else if ((rd1_i [DW - 1] > rd2_i [DW - 1])) branch_o = 1;
+                    else if ((rd1_i [DW - 1] == 0'b0) && (rd2_i [DW - 1] == 0'b0)) begin
+                        if (rd1_i [DW - 2:0] > rd2_i [DW - 2:0]) branch_o = 0;
+                        else branch_o = 1;
+                    end
+                    else if ((rd1_i [DW - 1] == 1'b1) && (rd2_i [DW - 1] == 1'b1)) begin
+                        if (rd1_i [DW - 2:0] > rd2_i [DW - 2:0]) branch_o = 0;
+                        else branch_o = 1;
+                    end
+                end //blt branch_o if rd1_i less than rd2_i
         3'b101: branch_o = rd1_i >= rd2_i ? 1'b1 : 1'b0; //bge branch_o if rd1_i greater than or equal than rd2_i
-        3'b110: begin
-            rd1_i = {{rd1_i[31]},{rd1_i}}; //sign extend rd1_i by 1 bit
-            rd2_i = {{1'b0},{rd2_i}}; //make rd2_i unsigned
-            branch_o = rd1_i < rd2_i ? 1'b1 : 1'b0; //bltu branch_o if rd2_i unisigned less than rd1_i
-        end
-        3'b111: begin
-            rd1_i = {{rd1_i[31]},{rd1_i}}; //sign extend rd1_i by 1 bit
-            rd2_i = {{1'b0},{rd2_i}}; //make rd2_i unsigned
-            branch_o = rd1_i >= rd2_i ? 1'b1 : 1'b0; //bgeu branch_o if rd2_i unisigned greater than rd1_i
-        end
+        3'b110: branch_o = rd1_i < rd2_i ? 1'b1 : 1'b0; //
+        3'b111: branch_o = rd1_i >= rd2_i ? 1'b1 : 1'b0; 
         default: branch_o=1'b0;    
     endcase
 end
