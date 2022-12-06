965b5b16defde6c3a815cf4cadfb996c39a3658f
Ana Dimoska <ad2121@ic.ac.uk>
2022-12-06 12:37:38 +0000

    changed order, fixed clock, fixed range

--- ram/ram.sv
+++ ram/ram.sv
@@ -2,49 +2,49 @@ module ram #(
     parameter WIDTH = 32, 
               DATA_W = 8
 ) (
-    input  logic  clk_i,
-    input  logic  write_en_i,
-    input  logic  AddrsCtrl_i,
-    input  logic [WIDTH-1:0] a_i,
-    input  logic [WIDTH-1:0] wd_i,
-    output logic [WIDTH-1:0] rd_o
+    input  logic                clk_i,
+    input  logic                write_en_i,
+    input  logic                AddrsCtrl_i,
+    input  logic [WIDTH-1:0]    a_i,
+    input  logic [WIDTH-1:0]    wd_i,
+    output logic [WIDTH-1:0]    rd_o
 );
 
-logic [DATA_W-1:0] ram_array [12'hFFF : 12'h0]; 
+    logic [DATA_W-1:0] ram_array [17'h1FFFF : 17'h0]; 
 
-initial begin 
-    $display  ("Loading ram.");
-    $readmemh ("data.mem", ram_array);
-end;
+    initial begin 
+        $display  ("Loading ram.");
+        $readmemh ("data.mem", ram_array);
+    end;
 
-always_ff @(posedge clk_i) begin
-    if (write_en_i==1 & AddrsCtrl_i==1) begin
-        ram_array[a_i] = wd_i[31:24];
-        ram_array[a_i + 1] = wd_i[23:16];
-        ram_array[a_i + 2] = wd_i[15:8];
-        ram_array[a_i + 3] = wd_i[7:0];
-        $display(ram_array[a_i],ram_array[a_i + 1],ram_array[a_i + 2],ram_array[a_i + 3]);
+    always_ff @(posedge clk_i) begin
+        if (write_en_i==1 & AddrsCtrl_i==1) begin
+            ram_array[a_i[16:0]] <= wd_i[7:0];
+            ram_array[a_i[16:0] + 1] <= wd_i[15:8];
+            ram_array[a_i[16:0] + 2] <= wd_i[23:16];
+            ram_array[a_i[16:0] + 3] <= wd_i[31:24];
+            $display(ram_array[a_i[16:0] + 3],ram_array[a_i[16:0] + 2],ram_array[a_i[16:0] + 1],ram_array[a_i[16:0]]);
+            
+        end
+
+        if (write_en_i==1 & AddrsCtrl_i==0) begin
+            ram_array[a_i[16:0]] <= wd_i[7:0]; 
+            $display (ram_array[a_i[16:0]]);
+        end    
+    end 
+
+
+    always_comb  begin
+        if (write_en_i==0 & AddrsCtrl_i==1)
+            rd_o = {ram_array[a_i[16:0] + 3],ram_array[a_i[16:0] + 2],ram_array[a_i[16:0] + 1],ram_array[a_i[16:0]]};
         
-    end
+        
+        if (write_en_i==0 & AddrsCtrl_i==0) begin
+            //sign extension
+            if (ram_array[a_i[16:0]][7]==0) rd_o = {{24'h0},{ram_array[a_i[16:0]]}};
+            if (ram_array[a_i[16:0]][7]==1) rd_o = {{24'hFFFFFF},{ram_array[a_i[16:0]]}};
+        end
 
-    if (write_en_i==1 & AddrsCtrl_i==0) begin
-        ram_array[a_i] = wd_i[7:0]; 
-        $display (ram_array[a_i]);
-    end    
-end 
-
-
-always_comb  begin
-    if (write_en_i==0 & AddrsCtrl_i==1)
-    rd_o <=  {ram_array[a_i],ram_array[a_i+1],ram_array[a_i+2],ram_array[a_i+3]};
-    
-    
-    if (write_en_i==0 & AddrsCtrl_i==0) begin
-        //sign extension
-        if (ram_array[a_i+3][7]==0) rd_o <= {{24'h0},{ram_array[a_i + 3]}};
-        if (ram_array[a_i+3][7]==1) rd_o <= {{24'hFFFFFF},{ram_array[a_i + 3]}};
-    end
-
-end 
+    end 
 
 endmodule
--- ram/test.cpp
+++ ram/test.cpp
@@ -20,7 +20,7 @@ int main(int argc,char **argv, char **env){
     top->a_i = 0;
     top->wd_i = 2864434397; //AABBCCDD
 
-    for (i=0; i<5; i++){ 
+    for (i=0; i<6; i++){ 
             
         for (clk=0; clk<2; clk++) {
                 tfp->dump (2*i+clk);
@@ -35,22 +35,24 @@ int main(int argc,char **argv, char **env){
         if (i==2){
             std::cout << top->rd_o << std::endl; //LOAD BYTE 11
             top->write_en_i = 1;
-            top->AddrsCtrl_i  = 0;
-            top->a_i = 8;   
+            top->AddrsCtrl_i  = 1;
+            top->a_i = 12;   
         }
         if (i==3){
-            top->a_i = 8;  
-            top->write_en_i = 0;
-            top->AddrsCtrl_i = 1;
-
+             std::cout << "*" << top->rd_o << std::endl;
+                                         //STORE BYTE AABBCCDD
+            top->write_en_i = 1;
+            top->AddrsCtrl_i  = 0;
+        }
         if (i==4){
-            std::cout << top->rd_o << std::endl;
+            std::cout << "*" << top->rd_o << std::endl;
+            top->write_en_i = 0;
+            top->AddrsCtrl_i  = 0;
+            std::cout << "final output" << top->rd_o << std::endl;
         }
 
-         //   top->a_i = 12;      //STORE BYTE AABBCCDD
-        //    top->write_en_i = 1;
-          //  top->AddrsCtrl_i  = 1;        
-        }
+                 
+        
          //STORE WORD AABBCCDD
 
     if (Verilated::gotFinish()) exit(0);
