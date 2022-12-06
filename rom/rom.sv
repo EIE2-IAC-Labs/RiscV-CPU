45f30de7f1d4b744fd97f90594b8d2ae58e986ec
Ana Dimoska <ad2121@ic.ac.uk>
2022-12-06 13:00:29 +0000

    range fixed

--- rom/rom.sv
+++ rom/rom.sv
@@ -1,13 +1,13 @@
 module rom #(
     parameter  WIDTH = 32, 
-               DATA_W = 8, //because we have PC+4 so 4 * 8 = 32 bits
-               PC_W = 12
+               DATA_W = 8 //because we have PC+4 so 4 * 8 = 32 bits
+               
 )(
-    input  logic [PC_W-1:0] a_i,
+    input  logic [WIDTH-1:0] a_i,
     output logic [WIDTH-1:0] rd_o
 );
 
-logic [DATA_W-1:0] rom_array [32'hfff : 32'h0]; 
+logic [DATA_W-1:0] rom_array [12'hfff : 12'h0]; 
 
 initial begin 
     $display ("Loading rom.");
@@ -15,7 +15,7 @@ initial begin
 end;
 
 always_comb begin 
-    rd_o = {rom_array[a_i],rom_array[a_i+1],rom_array[a_i+2],rom_array[a_i+3]}; // I do concatenation to get 32 bits( the instruction) from 8 bit parts
+    rd_o = {rom_array[a_i[11:0]],rom_array[a_i[11:0]+1],rom_array[a_i[11:0]+2],rom_array[a_i[11:0]+3]}; // I do concatenation to get 32 bits( the instruction) from 8 bit parts
 end
 
 endmodule
