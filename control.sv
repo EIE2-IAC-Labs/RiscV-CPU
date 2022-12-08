3455653560415d9f68d63f684f429360c2fc458b
Nik <nl621@ic.ac.uk>
2022-12-08 14:44:18 +0000

    Fixed JALR immSrc signal, added LH, LBU, LHU, SH compatability
    Replaced addrSelect with two new control signals, memType and memSign (see readme). Also added JALR output flag to drive mux in top. Added to testbench and tested.

--- control.sv
+++ control.sv
@@ -8,18 +8,23 @@ module control (
     output logic                        ALUsrc_o,       // selects immOp or regOp
     output logic [2:0]                  ImmSrc_o,       // control signal for sign extension
     output logic                        BranchSrc_o,    // gives control of PCsrc to branchctrl module
-    output logic                        addrSelect_o,   // toggles between word / byte addressing in RAM
     output logic                        ResultSrc_o,    // toggles between using ALUresult and ReadData from RAM. 
-    output logic                        jal_o           // overrides PCbranch and WD3
+    output logic [1:0]                  memType_o,      // memory address type
+    output logic                        memSign_o,      // determines signed or unsigned memory addressing
+    output logic                        jal_o,          // overrides WD3
+    output logic                        jalr_o          // overrides WD3 and PCbranch
 );
 
-    assign memWrite_en_o = (op_i ==  7'b0100011) ? 1'b1 : 1'b0;
-    assign BranchSrc_o = ((op_i == 7'b1100011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
-    assign ALUsrc_o = ((op_i == 7'b0010011) || (op_i == 7'b0000011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0; 
-    assign regWrite_en_o = ((op_i == 7'b0110011) || (op_i == 7'b0010011) || (op_i == 7'b0000011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
-    assign ResultSrc_o = (op_i == 7'b0000011) ? 1'b1 : 1'b0;
-    assign jal_o = ((op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
-    
+    always_comb begin
+        memWrite_en_o = (op_i ==  7'b0100011) ? 1'b1 : 1'b0;
+        BranchSrc_o = ((op_i == 7'b1100011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
+        ALUsrc_o = ((op_i == 7'b0010011) || (op_i == 7'b0000011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0; 
+        regWrite_en_o = ((op_i == 7'b0110011) || (op_i == 7'b0010011) || (op_i == 7'b0000011) || (op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
+        ResultSrc_o = (op_i == 7'b0000011) ? 1'b1 : 1'b0;
+        jal_o = ((op_i == 7'b1100111) || (op_i == 7'b1101111)) ? 1'b1 : 1'b0;
+        jalr_o = (op_i == 7'b1100111) ? 1'b1 : 1'b0;
+    end
+
     always_comb begin
 
         if (op_i == 7'b0010011) ImmSrc_o = 3'b000; // hack, taking this outside the case statement allow grouping cases below
@@ -51,26 +56,50 @@ module control (
             7'b1100011: ImmSrc_o = 3'b010; // B-Type instructions
 
             7'b0000011: begin // I-type load instructions
-                ALUctrl_o = 4'b1111;
+                ALUctrl_o = 4'b0000;
                 ImmSrc_o = 3'b000;
                 case (funct3_i)
-                    3'b000: // lb
-                        addrSelect_o = 1'b0;
-                    3'b010: // lw
-                        addrSelect_o = 1'b1;
-                    default: $display("Load Half, Load Half Unsigned and Load Byte Unsigned have not been implemented!!");
+                    3'b000: begin // lb
+                        memType_o = 2'b01;
+                        memSign_o = 1;
+                    end
+                    3'b001: begin // lh
+                        memType_o = 2'b10;
+                        memSign_o = 1;
+                    end
+                    3'b010: begin // lw
+                        memType_o = 2'b00;
+                        memSign_o = 1;
+                    end
+                    3'b100: begin // lbu
+                        memType_o = 2'b01;
+                        memSign_o = 0;
+                    end
+                    3'b101: begin // lhu
+                        memType_o = 2'b10;
+                        memSign_o = 0;
+                    end
+                    default: ;
                 endcase
             end
 
             7'b0100011: begin // S-type instructions
-                ALUctrl_o = 4'b1111;
+                ALUctrl_o = 4'b0000;
                 ImmSrc_o = 3'b001;
                 case (funct3_i)
-                    3'b000: // sb
-                        addrSelect_o = 1'b0;
-                    3'b010: // sw
-                        addrSelect_o = 1'b1;
-                    default: $display("Store Half has not been implemented!! ");
+                    3'b000: begin // sb
+                        memType_o = 2'b01;
+                        memSign_o = 0;
+                    end
+                    3'b001: begin // sh
+                        memType_o = 2'b10;
+                        memSign_o = 0;
+                    end
+                    3'b010: begin // sw
+                        memType_o = 2'b00;
+                        memSign_o = 0;
+                    end
+                    default: ;
                 endcase
             end 
             
@@ -78,7 +107,7 @@ module control (
             // 7'b0110111: // Load Upper Immediate
 
             7'b1100111: begin // Jump and Link Register
-                ImmSrc_o = 3'b100;
+                ImmSrc_o = 3'b000;
                 ALUctrl_o = 4'b1011; // ALU will forward SrcB to ALUresult, and set branch_o = 1
             end
 
--- control_tb.cpp
+++ control_tb.cpp
@@ -17,14 +17,16 @@ void display(Vcontrol* top){
 
     cout << "----- OUTPUTS ---------------------------" << endl << endl;
 
-    cout << "regwrite: " << bitset<1>(top->regWrite_en_o) << endl;
-    cout << "AluCtrl: " << bitset<4>(top->ALUctrl_o) << endl;
-    cout << "ALUsrc_o: " << bitset<1>(top->ALUsrc_o) << endl;
-    cout << "ImmSrc_o: " << bitset<3>(top->ImmSrc_o) << endl;
-    cout << "BranchSrc_o: " << bitset<1>(top->BranchSrc_o) << endl;
-    cout << "addrSelect_o: " << bitset<1>(top->addrSelect_o) << endl;
-    cout << "ResultSrc_o: " << bitset<1>(top->ResultSrc_o) << endl;
-    cout << "jal_o: " << bitset<1>(top->jal_o) << endl;
+    cout << "regwrite: \t" << bitset<1>(top->regWrite_en_o) << endl;
+    cout << "AluCtrl: \t" << bitset<4>(top->ALUctrl_o) << endl;
+    cout << "ALUsrc_o: \t" << bitset<1>(top->ALUsrc_o) << endl;
+    cout << "ImmSrc_o: \t" << bitset<3>(top->ImmSrc_o) << endl;
+    cout << "BranchSrc_o: \t" << bitset<1>(top->BranchSrc_o) << endl;
+    cout << "memType_o: \t" << bitset<2>(top->memType_o) << endl;
+    cout << "memSign_o: \t" << bitset<1>(top->memSign_o) << endl;
+    cout << "ResultSrc_o: \t" << bitset<1>(top->ResultSrc_o) << endl;
+    cout << "jal_o:   \t" << bitset<1>(top->jal_o) << endl;
+    cout << "jalr_o: \t" << bitset<1>(top->jalr_o) << endl;
 }
 
 int main(int argc, char **argv, char **env){
@@ -36,6 +38,8 @@ int main(int argc, char **argv, char **env){
     vector<int> funct3{0b000, 0b001, 0b010, 0b011, 0b100, 0b101, 0b110, 0b111};
     vector<int> funct3_load_store{0b000, 0b001, 0b010, 0b100, 0b101};
     vector<int> funct3_branch{0b000, 0b001, 0b100, 0b101, 0b110, 0b111};
+
+    cout << "\n \n +++++++++ R-TYPE INSTRUCTIONS ++++++++++ \n \n " << endl;
 
     top->op_i = 0b0110011; // Testing R-Type instructions
     for(int i = 0; i < 8; i++){
@@ -50,6 +54,8 @@ int main(int argc, char **argv, char **env){
         display(top);
     }
 
+    cout << "\n \n +++++++++ I-TYPE BITWISE INSTRUCTIONS ++++++++++ \n \n " << endl;
+
     top->op_i = 0b0010011;  // Testing I-Type Bitwise Instructions
     for(int i = 0; i < 8; i++){
         top->funct3_i = funct3[i];
@@ -63,12 +69,16 @@ int main(int argc, char **argv, char **env){
         display(top);
     }
 
+    cout << "\n \n +++++++++ I-TYPE LOAD INSTRUCTIONS ++++++++++ \n \n " << endl;
+
     top->op_i = 0b0000011;  // Testing I-Type Load Instructions
     for(int i = 0; i < 5; i++){
         top->funct3_i = funct3_load_store[i];
         top->eval();
         display(top);
     }
+
+    cout << "\n \n +++++++++ S-TYPE INSTRUCTIONS ++++++++++ \n \n " << endl;
 
     top->op_i = 0b0100011;  // Testing I-Type Store Instructions
     for(int i = 0; i < 3; i++){
@@ -77,6 +87,8 @@ int main(int argc, char **argv, char **env){
         display(top);
     }
 
+    cout << "\n \n +++++++++ B-TYPE INSTRUCTIONS ++++++++++ \n \n " << endl;
+
     top->op_i = 0b1100011;  // Testing Branch Instructions
     for(int i = 0; i < 6; i++){
         top->funct3_i = funct3_branch[i];
@@ -84,11 +96,13 @@ int main(int argc, char **argv, char **env){
         display(top);
     }
 
-    top->op_i = 0b1100111;
+    cout << "\n \n +++++++++ JAL INSTRUCTIONS ++++++++++ \n \n " << endl;
+
+    top->op_i = 0b1100111;  // jalr
     top->eval();
     display(top);
 
-    top->op_i = 0b1101111;
+    top->op_i = 0b1101111; // jal
     top->eval();
     display(top);
 
