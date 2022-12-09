0903a18fb381f8cb6a1901bdc9517082ad1c53ce
Ana Dimoska <ad2121@ic.ac.uk>
2022-12-09 11:20:29 +0000

    testesd store instructions decided to do some changes

--- ram/ram_new.cpp
+++ ram/ram_new.cpp
@@ -15,11 +15,11 @@ int main(int argc,char **argv, char **env){
     tfp->open ("ram_top.vcd");
 
     top->clk_i = 0;
-    top->write_en_i = 0;
+    top->write_en_i = 1;
     top->memtype_i = 2;
     top->memsign_i = 0;
     top->a_i = 0;
-    top->wd_i = 2864434397; //AABBCCDD
+    top->wd_i = 287454020;          //287454020;11223344            //2864434397; //AABBCCDD
 
     for (i=0; i<5; i++){ 
             
@@ -31,24 +31,34 @@ int main(int argc,char **argv, char **env){
         if (i==1){
             std::cout << top->rd_o << std::endl; //LOAD WORD DDCCBBAA
             std::cout << "clk 1 " << std::endl;
+            top->write_en_i = 0;
+            top->memtype_i = 3;
+            top->memsign_i = 1;
+            top->a_i = 3;
          
             
         }
         if (i==2){
-            top->a_i = 4;
+            top->a_i = 0;
             std::cout << top->rd_o << std::endl; //LOAD BYTE 11
-             std::cout << "clk 2 " << std::endl;
+            std::cout << "clk 2 " << std::endl;
+            top->write_en_i = 1;
+            top->memtype_i = 3;
+            top->memsign_i = 0;
               
         }
         if (i==3){
-            top->a_i = 8;  
-            std::cout << top->rd_o << std::endl; //LOAD BYTE 11
+             top->a_i = 6;  
+            //std::cout << top->rd_o << std::endl; //LOAD BYTE 11
              std::cout << "clk 3 " << std::endl;
+             top->write_en_i = 0;
+             top->memtype_i = 1;
+             top->memsign_i = 0;
         }   
 
         if (i==4){
             top->a_i = 12;  
-            std::cout << top->rd_o << std::endl;
+             std::cout << top->rd_o << std::endl;
              std::cout << "clk 4 " << std::endl;
         }
 
