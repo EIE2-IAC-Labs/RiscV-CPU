753577b9c5c52c3f2b03985ee0a527afdc0913ec
BenShort <benedictnjshort@gmail.com>
2022-12-09 12:58:40 +0000

    fetch_reg_file conforms with readme

--- fetch_reg_file.sv
+++ fetch_reg_file.sv
@@ -2,19 +2,19 @@ module fetch_reg_file #(
     parameter DATA_WIDTH = 32
 )(
     input logic                         clk,
-    input logic [DATA_WIDTH-1:0]        RD_i,
-    input logic [DATA_WIDTH-1:0]        PCF_i,
-    input logic [DATA_WIDTH-1:0]        PCPlus4F_i,
+    input logic [DATA_WIDTH-1:0]        instrD_i,
+    input logic [DATA_WIDTH-1:0]        incPCD_i,
+    input logic [DATA_WIDTH-1:0]        PCD_i,
 
-    output logic [DATA_WIDTH-1:0]       InstrD_o,
-    output logic [DATA_WIDTH-1:0]       PCD_o,
-    output logic [DATA_WIDTH-1:0]       PCPlus4D_o
+    output logic [DATA_WIDTH-1:0]       instrE_o,
+    output logic [DATA_WIDTH-1:0]       incPCE_o,
+    output logic [DATA_WIDTH-1:0]       PCE_o
 );
 
 always_ff @(posedge clk) begin
-    InstrD_o <= RD_i;
-    PCD_o <= PCF_i;
-    PCPlus4D_o <= PCPlus4F_i;
+    instrE_o <= instrD_i;
+    incPCE_o <= incPCD_i;
+    PCE_o <= PCD_i;
 end
 
 endmodule
