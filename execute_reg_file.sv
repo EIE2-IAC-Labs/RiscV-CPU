c3b407d89a4acabb18999a7f3479dfe26874629f
BenShort <benedictnjshort@gmail.com>
2022-12-09 13:23:39 +0000

    update reg_files from branch

--- execute_reg_file.sv
+++ execute_reg_file.sv
@@ -1,34 +1,26 @@
 module execute_reg_file #(
     parameter DATA_WIDTH = 32
 )(
-input logic                     clk,
-input logic                     JALRD_i,
-input logic                     ResultSrcE_i
-input logic                     MemWriteE_i
-input logic [DATA_WIDTH-1:0]    ALUResult_i
-input logic [DATA_WIDTH-1:0]    WriteDataE_i
-input logic [3:0]               RdE_i
-input logic [DATA_WIDTH-1:0]    PcPlus4E_i
+    input logic                     clk,
+    input logic                     resultSRCD_i,
+    input logic                     memWriteD_i,
+    input logic [DATA_WIDTH-1:0]    addrSelectD_i,
+    input logic [DATA_WIDTH-1:0]    ALUresultD_i,
+    input logic [DATA_WIDTH-1:0]    RD2D_i,
 
-output logic                     JALRD_i,
-
-output logic                    RegWriteM_o
-output logic                    ResultSrcM_o
-output logic                    MemWriteM_o
-output logic [DATA_WIDTH-1:0]   ALUResultM_o
-output logic [DATA_WIDTH-1:0]   WriteDataM_o
-output logic [3:0]              RdM_o
-output logic [DATA_WIDTH-1:0]   PCPlus4M_o
+    output logic                    resultSRCE_o,
+    output logic                    memWriteE_o,
+    output logic [DATA_WIDTH-1:0]   addrSelectE_o,
+    output logic [DATA_WIDTH-1:0]   ALUresultE_o,
+    output logic [DATA_WIDTH-1:0]   RD2E_o
 );
 
-always_ff @(posedge clk) begin
-    RegWriteM_o <= RegWriteE_i;
-    ResultSrcM_o <= ResultSrcE_i;
-    MemWriteM_o <= MemWriteE_i;
-    ALUResultM_o <= ALUResult_i;
-    WriteDataM_o <= WriteDataE_i;
-    RdM_o <= RdE_i;
-    PCPlus4M_o <= PcPlus4E_i;
+always_ff @(negedge clk) begin
+    resultSRCE_o <= resultSRCD_i;
+    memWriteE_o <= memWriteD_i;
+    addrSelectE_o <= addrSelectD_i;
+    ALUresultE_o <= ALUresultD_i;
+    RD2E_o <= RD2D_i;
 end
 
 endmodule
