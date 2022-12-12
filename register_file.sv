90142baea7e55ca33bc4fa09f20e8da781e482b6
Benedict Short <95176485+BenShort@users.noreply.github.com>
2022-12-12 15:26:38 +0000

    protect register zero

--- register_file.sv
+++ register_file.sv
@@ -18,7 +18,9 @@ module register_file #(
 logic [DATA_WIDTH-1:0] register_array [2**ADDRESS_WIDTH-1:0];
 
 always_ff @(posedge clk) begin
-    if (WE3_i) register_array[AD3_i] <= WD3_i;
+    if(AD3_i != {{ADDRESS_WIDTH-1:0}0}) begin
+        if (WE3_i) register_array[AD3_i] <= WD3_i;
+    end
     if(TRIGGER_i == 1'b1) register_array[5] <= 1;
    
 end
