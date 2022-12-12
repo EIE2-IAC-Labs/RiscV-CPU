d3fec746814d7b998383b669e6728246a1a9e87e
Nik <nl621@ic.ac.uk>
2022-12-12 15:33:27 +0000

    Fixed typo

--- register_file.sv
+++ register_file.sv
@@ -24,9 +24,7 @@ initial begin
     end
 end
 always_ff @(posedge clk) begin
-    if(AD3_i != {{ADDRESS_WIDTH-1:0}0}) begin
-        if (WE3_i) register_array[AD3_i] <= WD3_i;
-    end
+    if(WE3_i & AD3_i != 0) register_array[AD3_i] <= WD3_i;
     if(TRIGGER_i == 1'b1) register_array[5] <= 1;
 end
 always_comb begin 
