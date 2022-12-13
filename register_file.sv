629a95ee10c9bd1cdc1ed5fde093d4ac890989b5
BenShort <benedictnjshort@gmail.com>
2022-12-13 11:11:39 +0000

    update extend and register_file

--- register_file.sv
+++ register_file.sv
@@ -17,24 +17,15 @@ module register_file #(
 
 logic [DATA_WIDTH-1:0] register_array [2**ADDRESS_WIDTH-1:0];
 
-
-initial begin
-    for (int i = 0; i < $size(register_array); i++) begin
-        register_array[i] = 32'b0;
-    end
-end
 always_ff @(posedge clk) begin
-    if(WE3_i & AD3_i != 0) register_array[AD3_i] <= WD3_i;
+    if(AD3_i != {{ADDRESS_WIDTH-1}{1'b0}}) begin
+        if (WE3_i) register_array[AD3_i] <= WD3_i;
+    end
     if(TRIGGER_i == 1'b1) register_array[5] <= 1;
 end
 always_comb begin 
     RD1_o = register_array[AD1_i];
-    RD2_o = register_array[AD2_i];
-    // $display("Write Addr: %h", AD3_i);
-    // $display("Write data: %h", WD3_i);
-    // $display("Write a0: %h", register_array[10]);
-    // $display("Write enable: %h", WE3_i);
-    
+    RD2_o = register_array[AD2_i];    
 end
 
 assign  a0_o = register_array[10];
