5562c3acfe286c6185bc731298068dbc6881467d
BenShort <benedictnjshort@gmail.com>
2022-12-13 10:59:53 +0000

    updated write protection to x0

--- register_file.sv
+++ register_file.sv
@@ -24,17 +24,14 @@ initial begin
     end
 end
 always_ff @(posedge clk) begin
-    if (WE3_i) register_array[AD3_i] <= WD3_i;
+    if(AD3_i != {{ADDRESS_WIDTH-1}{1'b0}}) begin
+        if (WE3_i) register_array[AD3_i] <= WD3_i;
+    end
     if(TRIGGER_i == 1'b1) register_array[5] <= 1;
 end
 always_comb begin 
     RD1_o = register_array[AD1_i];
-    RD2_o = register_array[AD2_i];
-    // $display("Write Addr: %h", AD3_i);
-    // $display("Write data: %h", WD3_i);
-    // $display("Write a0: %h", register_array[10]);
-    // $display("Write enable: %h", WE3_i);
-    
+    RD2_o = register_array[AD2_i];    
 end
 
 assign  a0_o = register_array[10];
