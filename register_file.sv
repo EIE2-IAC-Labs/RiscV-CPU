1a23d21faf1c1aac70a5ed8facdb83c39aab47b3
BenShort <benedictnjshort@gmail.com>
2022-12-13 18:04:00 +0000

    fixed register_file x0 bitwidth protection

--- register_file.sv
+++ register_file.sv
@@ -18,7 +18,7 @@ module register_file #(
 logic [DATA_WIDTH-1:0] register_array [2**ADDRESS_WIDTH-1:0];
 
 always_ff @(posedge clk) begin
-    if(AD3_i != {{ADDRESS_WIDTH-1}{1'b0}}) begin
+    if(AD3_i != {ADDRESS_WIDTH{1'b0}}) begin
         if (WE3_i) register_array[AD3_i] <= WD3_i;
     end
     if(TRIGGER_i == 1'b1) register_array[5] <= 1;
