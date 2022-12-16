ea1ee3a88e89cda23e419ce285643deb6ddc2881
Nik <nl621@ic.ac.uk>
2022-12-16 15:44:45 +0000

    Changed frequency of display for pdf

--- cpu_tb.cpp
+++ cpu_tb.cpp
@@ -7,7 +7,7 @@
 #include <sstream>
 
 #include "vbuddy.cpp"     // include vbuddy code
-#define MAX_SIM_CYC 2000  // max simulation cycles
+#define MAX_SIM_CYC 1000000  // max simulation cycles
 
 int main(int argc, char **argv, char **env) {
   int simcyc;     // simulation clock count
@@ -24,7 +24,7 @@ int main(int argc, char **argv, char **env) {
  
   // init Vbuddy
   if (vbdOpen()!=1) return(-1);
-  vbdHeader("F1 Program");
+  vbdHeader("PDF Program");
   vbdSetMode(1);        // Flag mode set to one-shot
   vbdBar(0); //clear lights
 
@@ -47,21 +47,21 @@ int main(int argc, char **argv, char **env) {
     ///// f1.sh and pdf.sh will format respectively //////
     //////////////////////////////////////////////////////
 
-    /*pdfstartX 
-       if (simcyc > 800000 && simcyc % 4 == 0){  // plot every 4th, convenient for pipelining due to nops in display
+    /*pdfstart*/ 
+       if (simcyc > 800000 && simcyc % 8 == 0){  // plot every 4th, convenient for pipelining due to nops in display
        vbdPlot(int(top->data_out), 0, 255);
        vbdCycle(simcyc);
      }
-    Ypdfend*/
+    /*pdfend*/
 
-    /*f1start*/
+    /*f1startX
     vbdCycle(simcyc);
     top->trigger_i = vbdFlag();
     vbdBar(top->data_out & 0xFF);
     vbdHex(3,(int(top->data_out)>>8)&0xF);
     vbdHex(2,(int(top->data_out)>>4)&0xF);
     vbdHex(1,int(top->data_out)&0xF);
-    /*f1end*/
+    Yf1end*/
 }
 
 vbdClose();     // ++++
